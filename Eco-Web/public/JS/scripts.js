627f341beb74c2b6dcc3c758237e98914d805779
Pierrechami <pierrechaminade17@gmail.com>
2022-03-24 11:06:56 +0100

    Filtre formation
    Mise en place du filtre pour les formations, pour rechercher une formation en particulier

---
+++ Eco-Web/public/JS/scripts.js
@@ -0,0 +1,18 @@
+/*$(document).ready(function(){
+    $("#myInput").on("keyup", function() {
+        var value = $(this).val().toLowerCase();
+        $("#myTable tr").filter(function() {
+            $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+        });
+    });
+});
+ */
+
+$(document).ready(function () {
+    $("#search").on("keyup", function () {
+        var value = $(this).val().toLowerCase();
+        $(".card").filter(function () {
+            $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+        });
+    });
+});
