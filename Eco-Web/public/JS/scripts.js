f86a77b4117dfb0f9f07e1e8e4f23132b88a58d5
Pierrechami <pierrechaminade17@gmail.com>
2022-04-08 19:40:59 +0200

    PROGRESS
    Mis en place pour les apprenants d'un btn formation et d'un btn formation en cours, afin qu'un apprenant puisse visualiser les formations qu'il a commencées. Quand un apprenant clique sur "leçon terminée" alors la formation qui est rattachée à la leçon passe en formation en cours.

--- Eco-Web/public/JS/scripts.js
+++ Eco-Web/public/JS/scripts.js
@@ -1,8 +1,55 @@
 $(document).ready(function () {
+
+    let colorGray = "#6c757d"
+
+    let btnFormations = document.getElementById('btn-formations');
+    let btnFormationsEnCours = document.getElementById('btn-formations-en-cours');
+    let btnFormationsTerminée = document.getElementById('btn-formations-terminées');
+
+    let formations = document.getElementById('formations');
+    let formationsEnCours = document.getElementById('formationsEnCours')
+ //   let formationsTerminée = document.getElementById('formationsTerminée')
+
     $("#search").on("keyup", function () {
         var value = $(this).val().toLowerCase();
         $(".card").filter(function () {
             $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
         });
     });
+
+    btnFormations.addEventListener('click' , () => {
+        formations.style.display = "block";
+        btnFormations.style.backgroundColor = colorGray;
+        btnFormations.style.color = "white";
+
+        formationsEnCours.style.display = "none";
+        btnFormationsEnCours.style.backgroundColor = "white"
+        btnFormationsEnCours.style.color = colorGray
+
+        //  formationsTerminée.style.display = "none";
+        //  btnFormationsTerminée.style.backgroundColor = "white"
+        //  btnFormationsTerminée.style.color = colorGray
+    })
+
+
+
+    btnFormationsEnCours.addEventListener('click' , () => {
+        formationsEnCours.style.display = "block";
+        btnFormationsEnCours.style.backgroundColor = colorGray
+        btnFormationsEnCours.style.color = "white"
+
+        formations.style.display = "none";
+        btnFormations.style.backgroundColor = "white"
+        btnFormations.style.color = colorGray
+
+
+        //   formationsTerminée.style.display = "none";
+        //  btnFormationsTerminée.style.backgroundColor = "white"
+        //  btnFormationsTerminée.style.color = colorGray
+   })
+
+
+
+
+
 });
